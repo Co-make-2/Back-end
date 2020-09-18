b44edf5a61479694c312f090862b92df1479b5c4
Simone Roy <simonecarolynr@gmail.com>
2020-09-17 19:03:34 -0600

    deploying

--- data/seeds/04-user-profile_listings.js
+++ data/seeds/04-user-profile_listings.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('user-profile_listings').del()
+  return knex('user-profiles_listings').del()
     .then(function () {
       // Inserts seed entries
-      return knex('user-profile_listings').insert([
+      return knex('user-profiles_listings').insert([
         {userId:"1", listingsId:"1"},
         {userId:"1", listingsId:"2"},
         {userId:"2", listingsId:"3"},
