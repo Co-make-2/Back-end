88df4e52f8cdf4d1f69d5ca5773317e548e184d9
Simone Roy <simonecarolynr@gmail.com>
2020-09-21 17:14:49 -0600

    git pull

--- data/seeds/03-listings.js
+++ data/seeds/03-listings.js
@@ -5,17 +5,10 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('listings').insert([
-<<<<<<< HEAD
-        {listingsId: 1, userId:"", listingsName:"", description:"", location:"", city:"", state:"", zipCode:"", upVotes: 1 },
-        {listingsId: 2, userId:"", listingsName:"", description:"", location:"", city:"", state:"", zipCode:"", upVotes: 1},
-        {listingsId: 3, userId:"", listingsName:"", description:"", location:"", city:"", state:"",zipCode:"", upVotes: 1},
-        {listingsId: 4, userId:"", listingsName:"", description:"", location:"", city:"", state:"",zipCode:"", upVotes: 1}
-=======
-        {listingsId: 1, userId:"1", listingsName:"obstructed view", description:"overgrown hedges exiting store parking lot", location:"224 Main Street", city:"Sarasota", state:"PA", zipCode:"56759", upVotes: 2 },
+        {listingsId: 1, userId:"1", listingsName:"obstructed view", description:"overgrown hedges exiting store parking lot", location:"224 Main Street", city:"Sarasota", state:"PA", zipCode:"56759", upVotes: 2},
         {listingsId: 2, userId:"1", listingsName:"pothole", description:"large deep pothole ", location:"intersection of Smith Street and Main Street", city:"Sarasota", state:"PA", zipCode:"56759", upVotes: 1},
         {listingsId: 3, userId:"2", listingsName:"injured deer", description:"deer with a broken leg", location:"near the highway", city:"Billings", state:"MT",zipCode:"55555", upVotes: 1},
         {listingsId: 4, userId:"3", listingsName:"tree in road", description:"tree in the road on 4th street", location:"4th Street", city:"Billings", state:"MT",zipCode:"77777", upVotes: 1}
->>>>>>> 9921383967acfe2b9fb2bb2ba3b7e2c24ceba7dc
       ]);
     });
 };
--- data/seeds/user-profiles.js
+++ data/seeds/user-profiles.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('user-profiles').insert([
-        {userId: 1, username: 'a.dunn406', name: 'Albert Dunn', location: 'Sarasota, PA', skills: 'Animal control, Pest control', listings: ''},
-        {userId: 2, username: 'jdoe123', name: 'Jennifer Doenhauer', location: 'Billings, MT', skills: 'Carpentry, Tree Removal, Operating large machnery', listings: ''},
-        {userId: 3, username: 'emo.g0rl666', name: 'Stacey Ahbrams', location: 'Billings, MT', skills: 'Skinning deer, Looking sad, Yelling at my step-dad Steve', listings: ''}
+        {userId: 1, username: 'a.dunn406', name: 'Albert Dunn', city: 'Sarasota', state: 'PA', skills: 'Animal control, Pest control', listings: ''},
+        {userId: 2, username: 'jdoe123', name: 'Jennifer Doenhauer', city: 'Billings', state: 'MT', skills: 'Carpentry, Tree Removal, Operating large machnery', listings: ''},
+        {userId: 3, username: 'emo.g0rl666', name: 'Stacey Ahbrams', city: 'Billings', state: 'MT', skills: 'Skinning deer, Looking sad, Yelling at my step-dad Steve', listings: ''}
       ]);
     });
 };
