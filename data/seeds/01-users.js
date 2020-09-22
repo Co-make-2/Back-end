a64919035756363ea015054711edf8aba1f9ce19
Chris Forrest <redrider_cr@yahoo.com>
2020-09-22 19:36:52 -0400

    added hashedpasswords to seeds to make them functional

---
+++ data/seeds/01-users.js
@@ -0,0 +1,15 @@
+const hashedPassword = $2a$15$KJYjWGENwpdazhjsv6ESSuRaFLm9qjBWzbqww1T/KIPbboGSbT.eK;
+
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {username: 'a.dunn406', password: 'hashedPassword'},
+        {username: 'jdoe123', password: 'hashedPassword'},
+        {username: 'emo.g0rl666', password: 'hashedPassord'}
+      ]);
+    });
+};
